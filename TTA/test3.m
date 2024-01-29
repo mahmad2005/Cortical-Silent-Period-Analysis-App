2ad9af31b32051a85584ee3517df0f58d61a0980
MARUF AHMAD <m.ahmad2005@gmail.com>
2024-01-29 03:23:12 -0600

    TTA CSP updated. now in workable condition

--- TTA/test3.m
+++ TTA/test3.m
@@ -37,3 +37,16 @@ disp('Last Values of Each Sequence Above 3: ');
 disp(lastValues);
 disp('Indices of These Last Values: ');
 disp(lastIndices);
+
+
+x = [10, 20, 30, 40, 50, 60, 70, 80, 90];
+y = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+% Desired y value
+y_target = 5.5;
+
+% Interpolate to find the corresponding x value
+x_target = interp1(y, x, y_target, 'linear');
+
+% Display the result
+disp(['The interpolated value of x when y is ', num2str(y_target), ' is: ', num2str(x_target)]);
